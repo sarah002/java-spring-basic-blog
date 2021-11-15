e3c6c027dbf113600d46526bda7729187d926949
sarah002 <33877944+sarah002@users.noreply.github.com>
2021-11-15 14:21:27 +0000

    post details page

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -16,6 +16,11 @@ public class PostRepository {
     }
 
     public Post findById(Long id) {
+        for (Post post : ALL_POSTS) {
+            if (post.getId().equals(id)) {
+                return post;
+            }
+        }
         return null;
     }
 
